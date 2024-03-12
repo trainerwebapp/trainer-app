fd5f51dbf35db3a1f53400867d18feb6a774f3ab
trainerwebapp <trainer.webapp@gmail.com>
2024-03-12 03:40:21 +0000

    feat: update navbar headings

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -29,8 +29,8 @@ export default function Navbar() {
   const { data: session } = useSession();
 
   const navigation = [
-    { name: 'Home', href: '/' },
-    { name: 'Mission History', href: '/mission-history' },
+    { name: 'Current Mission', href: '/' },
+    { name: 'Completed missions', href: '/mission-history' },
   ].map((route: any) => ({
     ...route,
     current: router.pathname === route.href,
